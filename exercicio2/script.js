8ce1eb714f25f8f9a855052d92a17548e57840f8
Seu nome <jacksonsouza.consultor@gmail.com>
2022-11-01 21:44:23 -0300

    finalização exercicio

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,9 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+let minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log("quantidade de caracteres antes da alteração é: " + minhaString.length)
+minhaString = minhaString.trim()
+console.log("quantidade de caracteres após a alteração é: " + minhaString.length)
+minhaString = minhaString.replace("________","sticioso")
+console.log(minhaString)
+
+
+
